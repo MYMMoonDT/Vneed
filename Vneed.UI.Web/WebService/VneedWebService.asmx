7d2c9d992d4d9dcde502f6bdd747c1f937c75118
YI ZHENFEI <yizhenfei@gmail.com>
2012-10-21 22:52:03 +0800

    d

---
+++ Vneed.UI.Web/WebService/VneedWebService.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="VneedWebService.asmx.cs" Class="Vneed.UI.Web.WebService.VneedWebService" %>
---
+++ Vneed.UI.Web/WebService/VneedWebService.asmx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace Vneed.UI.Web.WebService
+{
+    /// <summary>
+    /// Summary description for VneedWebService
+    /// </summary>
+    [WebService(Namespace = "http://vneed.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class VneedWebService : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+    }
+}
